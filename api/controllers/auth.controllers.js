07afeebeae33ac3453917f28d026857d96c64bdf
LuqmanKhan <luqmnkn@gmail.com>
2026-05-14 22:11:43 +0500

    user registeration completed

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -4,25 +4,53 @@ import prisma from '../lib/prisma.js';
 export const register = async (req, res) => {
  const { username , email , password} = req.body
 
-
-const hashPassword = await bcrypt.hash(password, 10);
-
-
-const newUser = await prisma.user.create({
-data : {
-    username,
-    email,
-    password : hashPassword
+try {
+  
+  const hashPassword = await bcrypt.hash(password, 10);
+  
+  
+  const newUser = await prisma.user.create({
+  data : {
+      username,
+      email,
+      password : hashPassword
+  }
+  
+  
+  })
+  
+  console.log(newUser)
+  res.send(201).json({message : "User registered successfully"});
+} catch (error) {
+  console.error(error);
+  res.status(500).json({message : "Internal server error"});
 }
 
+}
+export const login = async (req, res) => {
 
+const { email , password } = req.body;
+//if user exists in database
+const user = await prisma.user.findUnique({
+  where : {
+    username
+  }
 })
 
-console.log(newUser)
-res.send('This is the register endpoint');
-
+if(!user){
+  return res.status(401).json({message : "invalid credentials"});
 }
-export const login = (req, res) => {
+
+
+
+//check if password is correct
+
+const isPasswordValid = await bcrypt.compare(password, user.password);
+
+if(!isPasswordValid){
+  return res.status(401).json({message : "invalid credentials"});
+}
+
   res.send('This is the login endpoint');
 }
 
